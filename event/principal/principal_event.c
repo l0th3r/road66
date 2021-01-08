5990c72653a5dfee25a621c6b1913613faf18550
L0th3r <alfabetatango9@gmail.com>
2021-01-08 10:21:17 +0100

    event fixed

--- event/principal/principal_event.c
+++ event/principal/principal_event.c
@@ -70,9 +70,9 @@ void pe_event_0()
             {
                 en_mod_food(+start_food);
                 els_is_inventory = 1;
-                current = 1;*/
+                current = 1;
             }
-            break;
+            break;*/
         case 1:
             temp = ui_choice("Leave your apartment.", "Check one last time for your stuff.", "/", "/");
             if (temp == 0)
